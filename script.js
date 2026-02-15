adab71698c14e6cc0fb00c33ffc9b6657f1303c8
satralnoorbyfmf <satralnoorbyfmf@gmail.com>
2026-02-15 17:24:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 window.onload = function() {
     const mainContent = document.getElementById('main-content');
-    
-    // সাইটটি লোড হওয়ার সময় একটু সুন্দর এনিমেশন
     setTimeout(() => {
         mainContent.style.opacity = "1";
         mainContent.style.transform = "translateY(0)";
