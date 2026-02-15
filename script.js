3d4401c316383fe5e4a7cb2bcfce340e593a18a8
satralnoorbyfmf <satralnoorbyfmf@gmail.com>
2026-02-15 13:00:14 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// ওয়েবসাইট লোড হলে এনিমেশন শুরু হবে
-window.onload = function() {
-    const mainContent = document.getElementById('main-content');
-    
-    setTimeout(() => {
-        mainContent.style.opacity = "1";
-        mainContent.style.transform = "translateY(0)";
-    }, 100);
-};
