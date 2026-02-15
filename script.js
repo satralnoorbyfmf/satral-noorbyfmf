6fc4d90321fb30bd43d4f863821228bba6a69d7c
satralnoorbyfmf <satralnoorbyfmf@gmail.com>
2026-02-15 13:09:06 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// সাইটটি সুন্দরভাবে ভেসে ওঠার জন্য
-window.onload = function() {
-    const mainContent = document.getElementById('main-content');
-    
-    setTimeout(() => {
-        mainContent.style.opacity = "1";
-        mainContent.style.transform = "translateY(0)";
-    }, 200);
-};
