c7206256d8b757da2d1550e9aecd45c08323ef62
satralnoorbyfmf <satralnoorbyfmf@gmail.com>
2026-02-16 13:08:09 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    const mainContent = document.getElementById('main-content');
-    setTimeout(() => {
-        mainContent.style.opacity = "1";
-        mainContent.style.transform = "translateY(0)";
-    }, 200);
-};
